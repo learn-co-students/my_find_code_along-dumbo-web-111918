6f358b55a8d803db7a8a0105b7f18b71240123ba
John Mark Redding <johnmarkredding@gmail.com>
2018-11-24 01:42:59 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  result = nil
+  while i < collection.size && result == nil
+    yield(collection[i]) ? result = collection[i] : result = nil
+    i += 1
+  end
+  result
 end
